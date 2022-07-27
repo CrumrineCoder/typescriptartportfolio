d4262efd98e44bbc18a3c2df80ccb195c2cfe2fa
Nicolas Crumrine <crumrinecoding@gmail.com>
2022-07-27 04:26:50 -0400

    Multiple images works

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import './App.css';
 import ArtBox from "./components/artBox"
 
 const test = require('./assets/tammy.png')
-
+const test2 = require('./assets/spidermaidasa.jpeg')
 function App() {
   // <CarcasoPortfolioBox imageName="test.jpg"></CarcasoPortfolioBox>
   // <CarcasoPortfolioBox imageName = {"./assets/tammy.png"} />
   return (
     <div className="App">
        <ArtBox imageName = {test}></ArtBox>
+       <ArtBox imageName = {test2}></ArtBox>
     </div>
   );
 }
