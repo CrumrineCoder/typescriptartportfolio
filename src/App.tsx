5a182ed295b4f83ea9d9294c9369f4be1312b628
Nicolas Crumrine <crumrinecoding@gmail.com>
2022-07-27 04:16:03 -0400

    moved functionality to new box

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,12 @@ import ArtBox from "./components/artBox"
 
 const test = require('./assets/tammy.png')
 
- interface CarcasoPortfolioBoxProps {
-    imageName: string;
- }
-
-function CarcasoPortfolioBox({imageName}: CarcasoPortfolioBoxProps) {
-  /*  
-      <img src ={require(imageName)} alt="Test" />
-  */
-  return (
-    <div>
-      test
-      <ArtBox></ArtBox>
-      <h1>{imageName}</h1>
-      <img src ={(imageName)} alt="Test" />
-    </div>
-  );
-}
-
 function App() {
   // <CarcasoPortfolioBox imageName="test.jpg"></CarcasoPortfolioBox>
   // <CarcasoPortfolioBox imageName = {"./assets/tammy.png"} />
   return (
     <div className="App">
-      test
-      <CarcasoPortfolioBox imageName = {test} />
+       <ArtBox imageName = {test}></ArtBox>
     </div>
   );
 }
--- src/components/artBox.tsx
+++ src/components/artBox.tsx
@@ -1,10 +1,15 @@
 
-function ArtBox() {
+ interface imageProps {
+    imageName: string;
+ }
+
+function ArtBox({imageName}: imageProps) {
     // <CarcasoPortfolioBox imageName="test.jpg"></CarcasoPortfolioBox>
     // <CarcasoPortfolioBox imageName = {"./assets/tammy.png"} />
     return (
       <div className="App">
         The art box is showing!
+        <img src ={(imageName)} alt="Test" />
       </div>
     );
   }
