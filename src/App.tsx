5ab6b220b2be21fe9b7224fd99ea805662b6b9dd
Nicolas Crumrine <crumrinecoding@gmail.com>
2022-07-27 20:17:54 -0400

    Working out props

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import './App.css';
 import ArtBox from "./components/artBox"
 
-const test = require('./assets/tammy.png')
-const test2 = require('./assets/spidermaidasa.jpeg')
+const TammyConfused = require('./assets/tammy.png')
+const AsheSaris = require('./assets/ashesaris.jpeg')
+
+
 function App() {
-  // <CarcasoPortfolioBox imageName="test.jpg"></CarcasoPortfolioBox>
-  // <CarcasoPortfolioBox imageName = {"./assets/tammy.png"} />
   return (
     <div className="App">
-       <ArtBox imageName = {test}></ArtBox>
-       <ArtBox imageName = {test2}></ArtBox>
+       <ArtBox imageName = {TammyConfused} artTitle={"Testing"} artDescription={"Here's the description!"}></ArtBox>
     </div>
   );
 }
--- src/components/artBox.tsx
+++ src/components/artBox.tsx
@@ -1,15 +1,18 @@
 
  interface imageProps {
     imageName: string;
+    artTitle: string;
+    artDescription: string; 
  }
 
-function ArtBox({imageName}: imageProps) {
+function ArtBox({imageName, artTitle, artDescription}: imageProps) {
     // <CarcasoPortfolioBox imageName="test.jpg"></CarcasoPortfolioBox>
     // <CarcasoPortfolioBox imageName = {"./assets/tammy.png"} />
     return (
       <div className="App">
-        The art box is showing!
         <img src ={(imageName)} alt="Test" />
+        <h1>{artTitle} </h1>
+        <p>{artDescription}</p>
       </div>
     );
   }
