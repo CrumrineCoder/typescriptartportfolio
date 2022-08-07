a7542fc4e8dbd82f22a5d7016a06e45e2de1589b
Nicolas Crumrine <crumrinecoding@gmail.com>
2022-08-07 14:14:56 -0400

    few things

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import './App.css';
-import ArtBox from "./components/artBox"
+import React from "react";
+import "./App.css";
+import ArtBox from "./components/artBox";
 
-const TammyConfused = require('./assets/tammy.png')
-const AsheSaris = require('./assets/ashesaris.jpeg')
-
+const TammyConfused = require("./assets/tammy.png");
+const AsheSaris = require("./assets/ashesaris.jpeg");
 
 function App() {
   return (
     <div className="App">
-       <ArtBox imageName = {TammyConfused} artTitle={"Testing"} artDescription={"Here's the description!"}></ArtBox>
+      <ArtBox
+        imageName={TammyConfused}
+        artTitle={"Testing"}
+        artDescription={"Here's the description!"}
+        style={"discord"}
+      ></ArtBox>
     </div>
   );
 }
--- src/components/artBox.tsx
+++ src/components/artBox.tsx
@@ -3,14 +3,15 @@
     imageName: string;
     artTitle: string;
     artDescription: string; 
+    style: string; 
  }
 
-function ArtBox({imageName, artTitle, artDescription}: imageProps) {
+function ArtBox({imageName, artTitle, artDescription, style}: imageProps) {
     // <CarcasoPortfolioBox imageName="test.jpg"></CarcasoPortfolioBox>
     // <CarcasoPortfolioBox imageName = {"./assets/tammy.png"} />
     return (
       <div className="App">
-        <img src ={(imageName)} alt="Test" />
+        <img className={style + "Image"} src ={(imageName)} alt="Test" />
         <h1>{artTitle} </h1>
         <p>{artDescription}</p>
       </div>
