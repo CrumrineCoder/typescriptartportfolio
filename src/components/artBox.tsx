c5d1b5be2f0dfc6e208154fd2e84b9086ebcc5bd
Nicolas Crumrine <crumrinecoding@gmail.com>
2022-08-09 18:08:05 -0400

    Trying to make content float left

--- src/components/artBox.tsx
+++ src/components/artBox.tsx
@@ -1,21 +1,22 @@
+interface imageProps {
+  imageName: string;
+  artTitle: string;
+  artDescription: string;
+  style: string;
+}
 
- interface imageProps {
-    imageName: string;
-    artTitle: string;
-    artDescription: string; 
-    style: string; 
- }
-
-function ArtBox({imageName, artTitle, artDescription, style}: imageProps) {
-    // <CarcasoPortfolioBox imageName="test.jpg"></CarcasoPortfolioBox>
-    // <CarcasoPortfolioBox imageName = {"./assets/tammy.png"} />
-    return (
-      <div className={style}>
-        <img className={"Image"} src ={(imageName)} alt="Test" />
+function ArtBox({ imageName, artTitle, artDescription, style }: imageProps) {
+  // <CarcasoPortfolioBox imageName="test.jpg"></CarcasoPortfolioBox>
+  // <CarcasoPortfolioBox imageName = {"./assets/tammy.png"} />
+  return (
+    <div className={style}>
+      <img className={"Image"} src={imageName} alt="Test" />
+      <div className="Text">
         <h1>{artTitle} </h1>
         <p>{artDescription}</p>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default ArtBox;
