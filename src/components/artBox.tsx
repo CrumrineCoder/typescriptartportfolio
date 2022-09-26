a34cd7006e099e6a136274e860decf0ff9b99878
Nicolas Crumrine <crumrinecoding@gmail.com>
2022-09-26 14:43:31 -0400

    Magnify ain't it chief

--- src/components/artBox.tsx
+++ src/components/artBox.tsx
@@ -3,19 +3,29 @@ interface imageProps {
   artTitle: string;
   artDescription: string;
   style: string;
-  date: string; 
+  date: string;
 }
 
-function ArtBox({ imageName, artTitle, artDescription, style, date }: imageProps) {
+function ArtBox({
+  imageName,
+  artTitle,
+  artDescription,
+  style,
+  date,
+}: imageProps) {
   // <CarcasoPortfolioBox imageName="test.jpg"></CarcasoPortfolioBox>
   // <CarcasoPortfolioBox imageName = {"./assets/tammy.png"} />
   return (
-    <div className={style}>
-      <img className={"portfolioProfilePictureImage"} src={imageName} alt="Test" />
+    <div className={"artBox " + style}>
+      <img
+        className={"portfolioProfilePictureImage"}
+        src={imageName}
+        alt="Test"
+      />
       <div className="portfolioProfilePictureTextbox">
         <div className="portfolioProfilePictureTextboxHeader">
-        <span className="portfolioProfilePictureTitle">{artTitle} </span>
-        <span className="portfolioProfilePictureDate">{date} </span>
+          <span className="portfolioProfilePictureTitle">{artTitle} </span>
+          <span className="portfolioProfilePictureDate">{date} </span>
         </div>
         <p className="portfolioProfilePictureDescription">{artDescription}</p>
       </div>
