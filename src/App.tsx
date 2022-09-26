acabefe40360200dc06df199bbf89671e52e4b80
Nicolas Crumrine <crumrinecoding@gmail.com>
2022-09-26 12:51:20 -0400

    Discord styling

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,21 @@ function App() {
       <ArtBox
         imageName={TammyConfused}
         artTitle={"Testing"}
-        artDescription={"Here's the description!"}
+        artDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper luctus mi, ut ornare leo suscipit non. Suspendisse fermentum tellus eget nulla blandit, sit amet pulvinar urna malesuada. Cras ac purus et tellus mollis sodales eget vel justo. Cras posuere tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sagittis mi et dapibus laoreet. Integer placerat lacus fringilla, suscipit dolor nec, porttitor leo. Nam in lobortis nisl. Aliquam erat volutpat. Praesent dapibus porttitor libero et suscipit."}
+        style={"discord"}
+        date={"11/20/2020"}
+      ></ArtBox>
+      <ArtBox
+        imageName={TammyConfused}
+        artTitle={"Testing"}
+        artDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper luctus mi, ut ornare leo suscipit non. Suspendisse fermentum tellus eget nulla blandit, sit amet pulvinar urna malesuada. Cras ac purus et tellus mollis sodales eget vel justo. Cras posuere tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sagittis mi et dapibus laoreet. Integer placerat lacus fringilla, suscipit dolor nec, porttitor leo. Nam in lobortis nisl. Aliquam erat volutpat. Praesent dapibus porttitor libero et suscipit."}
+        style={"discord"}
+        date={"11/20/2020"}
+      ></ArtBox>
+      <ArtBox
+        imageName={TammyConfused}
+        artTitle={"Testing"}
+        artDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper luctus mi, ut ornare leo suscipit non. Suspendisse fermentum tellus eget nulla blandit, sit amet pulvinar urna malesuada. Cras ac purus et tellus mollis sodales eget vel justo. Cras posuere tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sagittis mi et dapibus laoreet. Integer placerat lacus fringilla, suscipit dolor nec, porttitor leo. Nam in lobortis nisl. Aliquam erat volutpat. Praesent dapibus porttitor libero et suscipit."}
         style={"discord"}
         date={"11/20/2020"}
       ></ArtBox>
