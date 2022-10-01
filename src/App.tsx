e323c9a9f83cecd23daa90ca73788994edb0c78b
Nicolas Crumrine <crumrinecoding@gmail.com>
2022-10-01 15:13:04 -0400

    Profile pic containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,15 @@
-import React from "react";
 import "./App.scss";
-import ArtBox from "./components/artBox";
 import { Routes, Route, Link } from "react-router-dom";
-
-const TammyConfused = require("./assets/tammy.png");
-const AsheSaris = require("./assets/ashesaris.jpeg");
+import ProfilePictures from "./containers/profilepictures";
 
 function App() {
   return (
     <div className="App">
        <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<ProfilePictures />} />
         <Route path="about" element={<About />} />
       </Routes>
-      <ArtBox
-        imageName={TammyConfused}
-        artTitle={"Testing"}
-        artDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper luctus mi, ut ornare leo suscipit non. Suspendisse fermentum tellus eget nulla blandit, sit amet pulvinar urna malesuada. Cras ac purus et tellus mollis sodales eget vel justo. Cras posuere tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sagittis mi et dapibus laoreet. Integer placerat lacus fringilla, suscipit dolor nec, porttitor leo. Nam in lobortis nisl. Aliquam erat volutpat. Praesent dapibus porttitor libero et suscipit."}
-        style={"discord"}
-        date={"11/20/2020"}
-      ></ArtBox>
-      <ArtBox
-        imageName={AsheSaris}
-        artTitle={"Testing"}
-        artDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper luctus mi, ut ornare leo suscipit non. Suspendisse fermentum tellus eget nulla blandit, sit amet pulvinar urna malesuada. Cras ac purus et tellus mollis sodales eget vel justo. Cras posuere tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sagittis mi et dapibus laoreet. Integer placerat lacus fringilla, suscipit dolor nec, porttitor leo. Nam in lobortis nisl. Aliquam erat volutpat. Praesent dapibus porttitor libero et suscipit."}
-        style={"discord"}
-        date={"11/20/2020"}
-      ></ArtBox>
-      <ArtBox
-        imageName={TammyConfused}
-        artTitle={"Testing"}
-        artDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper luctus mi, ut ornare leo suscipit non. Suspendisse fermentum tellus eget nulla blandit, sit amet pulvinar urna malesuada. Cras ac purus et tellus mollis sodales eget vel justo. Cras posuere tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sagittis mi et dapibus laoreet. Integer placerat lacus fringilla, suscipit dolor nec, porttitor leo. Nam in lobortis nisl. Aliquam erat volutpat. Praesent dapibus porttitor libero et suscipit."}
-        style={"discord"}
-        date={"11/20/2020"}
-      ></ArtBox>
+     
     </div>
   );
 }
---
+++ src/containers/profilepictures.tsx
@@ -0,0 +1,44 @@
+import "../App.scss";
+import ArtBox from "../components/artBox";
+
+const TammyConfused = require("../assets/tammy.png");
+const AsheSaris = require("../assets/ashesaris.jpeg");
+
+
+function ProfilePictures() {
+  // <CarcasoPortfolioBox imageName="test.jpg"></CarcasoPortfolioBox>
+  // <CarcasoPortfolioBox imageName = {"./assets/tammy.png"} />
+  return (
+    <div className="ProfilePictures">
+      <ArtBox
+        imageName={TammyConfused}
+        artTitle={"Testing"}
+        artDescription={
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper luctus mi, ut ornare leo suscipit non. Suspendisse fermentum tellus eget nulla blandit, sit amet pulvinar urna malesuada. Cras ac purus et tellus mollis sodales eget vel justo. Cras posuere tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sagittis mi et dapibus laoreet. Integer placerat lacus fringilla, suscipit dolor nec, porttitor leo. Nam in lobortis nisl. Aliquam erat volutpat. Praesent dapibus porttitor libero et suscipit."
+        }
+        style={"discord"}
+        date={"11/20/2020"}
+      ></ArtBox>
+      <ArtBox
+        imageName={AsheSaris}
+        artTitle={"Testing"}
+        artDescription={
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper luctus mi, ut ornare leo suscipit non. Suspendisse fermentum tellus eget nulla blandit, sit amet pulvinar urna malesuada. Cras ac purus et tellus mollis sodales eget vel justo. Cras posuere tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sagittis mi et dapibus laoreet. Integer placerat lacus fringilla, suscipit dolor nec, porttitor leo. Nam in lobortis nisl. Aliquam erat volutpat. Praesent dapibus porttitor libero et suscipit."
+        }
+        style={"discord"}
+        date={"11/20/2020"}
+      ></ArtBox>
+      <ArtBox
+        imageName={TammyConfused}
+        artTitle={"Testing"}
+        artDescription={
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper luctus mi, ut ornare leo suscipit non. Suspendisse fermentum tellus eget nulla blandit, sit amet pulvinar urna malesuada. Cras ac purus et tellus mollis sodales eget vel justo. Cras posuere tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sagittis mi et dapibus laoreet. Integer placerat lacus fringilla, suscipit dolor nec, porttitor leo. Nam in lobortis nisl. Aliquam erat volutpat. Praesent dapibus porttitor libero et suscipit."
+        }
+        style={"discord"}
+        date={"11/20/2020"}
+      ></ArtBox>
+    </div>
+  );
+}
+
+export default ProfilePictures;
