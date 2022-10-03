e77f7d51e416f538343182a2e20f9de96642fcaf
Nicolas Crumrine <crumrinecoding@gmail.com>
2022-10-03 18:44:51 -0400

    Home Page Link

--- src/containers/HomePage.tsx
+++ src/containers/HomePage.tsx
@@ -1,11 +1,13 @@
 import "../App.scss";
 import { Link } from "react-router-dom";
+const TammyConfused = require("../assets/profilepics/tammy.png");
+
 function HomePage() {
   // <CarcasoPortfolioBox imageName="test.jpg"></CarcasoPortfolioBox>
   // <CarcasoPortfolioBox imageName = {"./assets/tammy.png"} />
   return (
     <div className="HomePage">
-       <Link to="/profilepictures">Profile Pictures</Link>
+       <Link to="/profilepictures"><img src={TammyConfused} className="homePageLinkImage"/></Link>
     </div>
   );
 }
