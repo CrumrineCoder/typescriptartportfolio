48914f9716f81b7ed80848db67f404fea8076c3d
Nicolas Crumrine <crumrinecoding@gmail.com>
2022-10-13 16:45:33 -0400

    Copy Update, Active Selector, border radius

--- src/components/profilepicselector.tsx
+++ src/components/profilepicselector.tsx
@@ -3,15 +3,28 @@ import "react-medium-image-zoom/dist/styles.css";
 interface imageProps {
   imageName: string;
   changeImage: (arg0: string) => void;
-  currentImage: string; 
+  currentImage: string;
 }
 
-function ProfilePicSelector({ imageName, changeImage, currentImage }: imageProps) {
+function ProfilePicSelector({
+  imageName,
+  changeImage,
+  currentImage,
+}: imageProps) {
   // <CarcasoPortfolioBox imageName="test.jpg"></CarcasoPortfolioBox>
   // <CarcasoPortfolioBox imageName = {"./assets/tammy.png"} />
   return (
     <div className={"profilePicSelector"}>
-      <img onClick={() => changeImage(imageName)} className={(currentImage == imageName) ? "activeSelector profilePicSelectorImage" : "profilePicSelectorImage"} src={imageName} alt="Test" />
+      <img
+        onClick={() => changeImage(imageName)}
+        className={
+          currentImage == imageName
+            ? "activeSelector profilePicSelectorImage"
+            : "profilePicSelectorImage"
+        }
+        src={imageName}
+        alt="Test"
+      />
     </div>
   );
 }
--- src/containers/profilepictures.tsx
+++ src/containers/profilepictures.tsx
@@ -178,7 +178,7 @@ function ProfilePictures() {
           I'm expanding to do commissions in my free time. I'm most interested in drawing cute and/or spooky things, and nothing overtly NSFW. 
         </div>
         <div className="ProfilePicturesLeftSideCTA">
-           Contact Me
+           Let's Chat
         </div>
       </div>
       <div className="ProfilePicturesRightSide">
