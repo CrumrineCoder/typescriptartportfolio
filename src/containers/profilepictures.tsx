b9f2538992609c92cc0f01dc945c59b2991b3004
Nicolas Crumrine <crumrinecoding@gmail.com>
2022-10-13 12:43:46 -0400

    structure css

--- src/containers/profilepictures.tsx
+++ src/containers/profilepictures.tsx
@@ -94,7 +94,7 @@ const profilePicsMasterData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper luctus mi, ut ornare leo suscipit non. Suspendisse fermentum tellus eget nulla blandit, sit amet pulvinar urna malesuada. Cras ac purus et tellus mollis sodales eget vel justo. Cras posuere tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sagittis mi et dapibus laoreet. Integer placerat lacus fringilla, suscipit dolor nec, porttitor leo. Nam in lobortis nisl. Aliquam erat volutpat. Praesent dapibus porttitor libero et suscipit.",
     style: "discord",
     date: "11/20/2020",
-  },
+  }, /*
   {
     imageName: lala2,
     artTitle: "Testing",
@@ -118,7 +118,7 @@ const profilePicsMasterData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper luctus mi, ut ornare leo suscipit non. Suspendisse fermentum tellus eget nulla blandit, sit amet pulvinar urna malesuada. Cras ac purus et tellus mollis sodales eget vel justo. Cras posuere tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sagittis mi et dapibus laoreet. Integer placerat lacus fringilla, suscipit dolor nec, porttitor leo. Nam in lobortis nisl. Aliquam erat volutpat. Praesent dapibus porttitor libero et suscipit.",
     style: "discord",
     date: "11/20/2020",
-  },
+  }, */
   {
     imageName: neeko,
     artTitle: "Testing",
