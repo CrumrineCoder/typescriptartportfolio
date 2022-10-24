75858d47a791b805e51c91398be7366a980091f2
Nicolas Crumrine <crumrinecoding@gmail.com>
2022-10-24 18:36:00 -0400

    Process formatting done

--- src/containers/CommissionInfo.tsx
+++ src/containers/CommissionInfo.tsx
@@ -4,148 +4,167 @@ function CommissionInfo() {
   return (
     <div className="CommissionInfo">
       <div className="CommissionInfoHeader">Commissions Info</div>
-      <div className="CommissionInfoSubHeader">Please review the pricing and terms below before sending a request to tamingthecarcasoan@gmail. Commission queue can be found here.</div>
+      <div className="CommissionInfoSubHeader">
+        Please review the pricing and terms below before sending a request to
+        tamingthecarcasoan@gmail. Commission queue can be found here.
+      </div>
       <div className="CommissionInfoApplicationContainer">
-        <p className="CommissionInfoApplicationBody">
-          Please send me an Email titled "COMMISSION - (Your name and Social
-          Media Username if you want to be tagged) [E-mail:
-          tamingthecarcasoan@gmail.com] With the below details. Alternatively,
-          you can send this over Twitter DMs
+        <p className="CommissionInfoApplicationSubHeader">
+          Commission Process:
         </p>
-        <p className="CommissionInfoApplicationBody">
-          I reserve the right to decline any commission request that I find
-          inappropriate and/or that doesn’t match my terms, skills or time.
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          I cannot give an exact time estimate at the moment as I’m working on a
-          lot of other things at the moment. If you want to know how many slots
-          are taken before you, please check my orders page. I like to take my
-          time with my commissions. I always try to get them done as soon as
-          possible, but I don’t work with a set amount of time when drawing.
-          Please be mindful that commissions further in the queue will take
-          longer to get started and finished. Please be aware that personal art
-          may happen between commissions.
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          If your illustration requires a due date, please specify. Rush orders
-          will require a fee.
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          To avoid delays please respond within a timely manner. Let me know if
-          you won't be available at any time so I know there will be a delayed
-          response.
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          Form to fill out these details to the best of your ability to help me
-          make the perfect art for you:
-        </p>
-        <ul className="CommissionInfoApplicationListContainer">
-          <li className="CommissionInfoApplicationListItem">Commission Type</li>
-          <li className="CommissionInfoApplicationListItem">Character Name</li>
-          <li className="CommissionInfoApplicationListItem">Race</li>
-          <li className="CommissionInfoApplicationListItem">
-            Age (or the human equivalent for elves and elder races). Broad
-            estimates are fine eg 20s, 30s
+        <ol className="CommissionInfoApplicationListContainer">
+          <li className="CommissionInfoApplicationBody">
+            After reading and agreeing to the Terms of Service, please send me
+            an Email titled "COMMISSION - (Your name and Social Media Username
+            if you want to be tagged) [E-mail: tamingthecarcasoan@gmail.com]
+            With the below details. Alternatively, you can send this over
+            Twitter DMs.
           </li>
-          <li className="CommissionInfoApplicationListItem">
-            Class if it's relevant to appearance or vibe (if fantasy)
+
+          <p className="CommissionInfoApplicationBody">
+            Time estimates on when I can respond to commission requests cannot
+            be given at the moment. Commissions will take as long as I feel is
+            required to do my best on each one. I will give frequent updates
+            during each one, and will never disappear from contact while I am
+            working on one. If your illustration requires a due date, please
+            specify. Rush orders will require a fee, and I won't always be able
+            to agree to one. You can see how many slots are taken before you on
+            my orders page, and commissions further in the queue will take
+            longer to get started. My personal life, art, and mental health
+            comes before starting a new commission.
+          </p>
+          <p className="CommissionInfoApplicationBody">
+            To avoid delays please respond within a timely manner. Let me know
+            if you won't be available at any time so I know there will be a
+            delayed response.
+          </p>
+
+          <p className="CommissionInfoApplicationBodyItalics">
+            Please include the following details in text:
+          </p>
+          <ul className="CommissionInfoApplicationListContainer">
+            <li className="CommissionInfoApplicationListItem">
+              Commission Type
+            </li>
+            <li className="CommissionInfoApplicationListItem">
+              Character Name
+            </li>
+            <li className="CommissionInfoApplicationListItem">
+              Age (or the human equivalent for elves and elder races). Broad
+              estimates are fine eg 20s, 30s
+            </li>
+            <li className="CommissionInfoApplicationListItem">
+              Class if it's relevant to appearance or vibe (if fantasy)
+            </li>
+            <li className="CommissionInfoApplicationListItem">
+              A concise summary of personality and desired expression. For me
+              this is one of the most important things for deciding if and how
+              to portray a character! A sentence or two is all that's needed.
+              It's perfectly ok to ask for a neutral expression.
+            </li>
+            <li className="CommissionInfoApplicationListItem">
+              You can add a little backstory if it helps you frame the mood, but
+              no big paragraphs of text please!
+            </li>
+            <li className="CommissionInfoApplicationListItem">Skin colour</li>
+            <li className="CommissionInfoApplicationListItem">Eye colour</li>
+            <li className="CommissionInfoApplicationListItem">
+              Hair colour and style
+            </li>
+            <li className="CommissionInfoApplicationListItem">Accessories</li>
+            <li className="CommissionInfoApplicationListItem">Scars</li>
+            <li className="CommissionInfoApplicationListItem">Outfit</li>
+            <li className="CommissionInfoApplicationListItem">
+              Pose (only for bigger pieces and illustrations). This can just be
+              a general idea if no specific pose is in mind.
+            </li>
+          </ul>
+          <p className="CommissionInfoApplicationBodyItalics">
+            Please include visual references according to the guidelines below:
+          </p>
+          <ul className="CommissionInfoApplicationListContainer">
+            <li className="CommissionInfoApplicationListItem">
+              These might include previous art you've commissioned, Pinterest
+              images, screenshots of characters in-game, WoW dressing room
+              links, or other art that inspired you (but don't ever use someone
+              else's OC design as your own! It's a huge no-no to copy someone's
+              character)
+            </li>
+            <li className="CommissionInfoApplicationListItem">
+              Note anything that might be difficult to see in references,
+              particularly scars
+            </li>
+            <li className="CommissionInfoApplicationListItem">
+              In-game lighting in screenshots can alter the colour of everything
+              drastically between different locations. Give your artist
+              references with neutral lighting, or provide colour swatches for
+              each feature
+            </li>
+            <li className="CommissionInfoApplicationListItem">
+              Faceclaims are extremely useful. These are photos of real people
+              who resemble your character. The final art won't be exactly like
+              the reference but gives an invaluable stepping off point in
+              knowing how you visualize your character
+            </li>
+            <li className="CommissionInfoApplicationListItem">
+              Don't worry if you have no art yet or can't find the perfect
+              matching picture. A reference does not have to match every aspect
+              of the character! You might provide one picture for hair, another
+              for eyes, another for face shape, another for expression and so
+              on.
+            </li>
+            <li className="CommissionInfoApplicationListItem">
+              If you have a particular scene in mind for an illustration, MS
+              paint doodles are perfect to show what you had in mind for pose
+              and composition
+            </li>
+            <li className="CommissionInfoApplicationListItem">
+              For accurate colours (eg for eyes or skin) you can use hex colour
+              codes, Pantone swatches, even screenshot a nice colour you found
+              in Google
+            </li>
+            <li className="CommissionInfoApplicationListItem">
+              One of the best ways people have sent me refs is via Google drive
+              or in Google docs. Put all the images in one character folder to
+              make it easy to navigate and there's the option to add notes to
+              any picture too. (Make sure to turn permissions on for viewing to
+              save time, it lets me see the pictures without any delays)
+            </li>
+          </ul>
+          <li className="CommissionInfoApplicationBody">
+            The concept of the artwork will be discussed, as well as the price
+            quote. Upon confirmation of price, I will send you an invoice via
+            Paypal. All payment is done 100% upfront. I will NOT send an invoice
+            unless you've agreed with the price.
           </li>
-          <li className="CommissionInfoApplicationListItem">
-            A concise summary of personality and desired expression. For me this
-            is one of the most important things for deciding if and how to
-            portray a character! A sentence or two is all that's needed. It's
-            perfectly ok to ask for a neutral expression.
+          <li className="CommissionInfoApplicationBody">
+            A thumbnail sketch(es) based on the client's description will be
+            done to determine general composition of the artwork.
           </li>
-          <li className="CommissionInfoApplicationListItem">
-            You can add a little backstory if it helps you frame the mood, but
-            no big paragraphs of text please!
+          <li className="CommissionInfoApplicationBody">
+            Once the thumbnail is approved, a detailed sketch will be created in order to
+            seek conceptual approval for the final piece. Revisions cannot be
+            made after final approval so please revise your commission carefully
+            and let me know if I make any mistakes during the process. I will
+            only accept up to 3 revisions once this sketch is made
+            (unless it was an error on my part!).
           </li>
-          <li className="CommissionInfoApplicationListItem">Skin colour</li>
-          <li className="CommissionInfoApplicationListItem">Eye colour</li>
-          <li className="CommissionInfoApplicationListItem">
-            Hair colour and style
+          <li className="CommissionInfoApplicationBody">
+            I will send you an Invoice through PayPal mail once the sketch is
+            approved. I will cancel the Invoice if I don’t hear back within 48
+            hours unless otherwise given notice. After this, I will provide at least 2 - 3
+            WIP to let you see each step (sketch, lines, colours) and you can
+            request for minor changes. 
           </li>
-          <li className="CommissionInfoApplicationListItem">Accessories</li>
-          <li className="CommissionInfoApplicationListItem">Scars</li>
-          <li className="CommissionInfoApplicationListItem">Outfit</li>
-          <li className="CommissionInfoApplicationListItem">
-            Pose (only for bigger pieces and illustrations). This can just be a
-            general idea if no specific pose is in mind.
+          <li className="CommissionInfoApplicationBody">
+            After the illustration is done, I will send you the thumbnail for
+            the final check.
           </li>
-        </ul>
-        <div className="CommissionInfoApplicationSubHeader">References</div>
-        <p className="CommissionInfoApplicationBody">
-          These might include previous art you've commissioned, Pinterest
-          images, screenshots of characters in-game, WoW dressing room links, or
-          other art that inspired you (but don't ever use someone else's OC
-          design as your own! It's a huge no-no to copy someone's character)
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          Note anything that might be difficult to see in references,
-          particularly scars
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          In-game lighting in screenshots can alter the colour of everything
-          drastically between different locations. Give your artist references
-          with neutral lighting, or provide colour swatches for each feature
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          Faceclaims are extremely useful. These are photos of real people who
-          resemble your character. The final art won't be exactly like the
-          reference but gives an invaluable stepping off point in knowing how
-          you visualize your character
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          Don't worry if you have no art yet or can't find the perfect matching
-          picture. A reference does not have to match every aspect of the
-          character! You might provide one picture for hair, another for eyes,
-          another for face shape, another for expression and so on.
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          If you have a particular scene in mind for an illustration, MS paint
-          doodles are perfect to show what you had in mind for pose and
-          composition
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          For accurate colours (eg for eyes or skin) you can use hex colour
-          codes, Pantone swatches, even screenshot a nice colour you found in
-          Google
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          One of the best ways people have sent me refs is via Google drive or
-          in Google docs. Put all the images in one character folder to make it
-          easy to navigate and there's the option to add notes to any picture
-          too. (Make sure to turn permissions on for viewing to save time, it
-          lets me see the pictures without any delays)
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          A thumbnail sketch(es) based on the client's description will be done
-          to determine general composition of the artwork.
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          Once it's approved, a detailed sketch will be created in order to seek
-          conceptual approval for the final piece. Revisions cannot be made
-          after final approval so please revise your commission carefully and
-          let me know if I make any mistakes during the process. I will only
-          accept up to 3 revisions once the initial sketch is made. I will
-          provide at least 2 - 3 wips to let you see each step (sketch, lines,
-          colours) and you can request for minor changes. After you approve it
-          and start coloring, only minor changes (color, shadows, or highlights)
-          will be accepted.
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          I will send you an Invoice through PayPal mail once the sketch is
-          approved. I will cancel the Invoice if I don’t hear back within 48
-          hours unless otherwise given notice
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          After the illustration is done, I will send you the thumbnail for the
-          final check, and I will send the High Res Version if there's no slight
-          adjustments to be made.
-        </p>
-        <p className="CommissionInfoApplicationBody">
-          I will send a high-res version of the art through email when finished.
-        </p>
+          <li className="CommissionInfoApplicationBody">
+            If approved, I will send a high-res version of the art through email when
+            finished.
+          </li>
+        </ol>
         <div className="CommissionInfoApplicationHeader"></div>
       </div>
 
@@ -200,6 +219,10 @@ function CommissionInfo() {
         <p className="CommissionInfoTOSBody">
           Whenever you post and share my work you should give me appropriate
           credit.
+        </p>
+        <p className="CommissionInfoApplicationBody">
+          I reserve the right to decline any commission request that I find
+          inappropriate and/or that doesn’t match my terms, skills or time.
         </p>
         <p className="CommissionInfoTOSBody">
           By paying the PayPal invoice, you agree to these Terms of Service.
