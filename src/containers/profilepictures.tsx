333892540310b9f363430e3ff274d5e97bb30311
Nicolas Crumrine <crumrinecoding@gmail.com>
2022-10-31 23:54:53 -0400

    starting work on ashe

--- src/containers/profilepictures.tsx
+++ src/containers/profilepictures.tsx
@@ -39,7 +39,7 @@ const profilePicsMasterData = [
   },
   {
     imageName: AsheSaris,
-    artTitle: "Testing",
+    artTitle: "Ashe",
     artDescription:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper luctus mi, ut ornare leo suscipit non. Suspendisse fermentum tellus eget nulla blandit, sit amet pulvinar urna malesuada. Cras ac purus et tellus mollis sodales eget vel justo. Cras posuere tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sagittis mi et dapibus laoreet. Integer placerat lacus fringilla, suscipit dolor nec, porttitor leo. Nam in lobortis nisl. Aliquam erat volutpat. Praesent dapibus porttitor libero et suscipit.",
     style: "discord",
