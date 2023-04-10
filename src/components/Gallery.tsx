a57ccb223d723b0c1a319c0d47d03dbaeb5157e4
Nicolas Crumrine <crumrinecoding@gmail.com>
2023-04-10 11:41:12 -0500

    Gallery text

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -18,60 +18,70 @@ function Gallery() {
       <div className="GalleryImagesContainer">
         <Zoom>
           <div className="GalleryItem">
+            <span className="GalleryText">Tami in style of Fire Emblem</span>
             <img className="GalleryImage" src={TammyPixelArt} />
           </div>
         </Zoom>
 
         <Zoom>
           <div className="GalleryItem">
+            <span className="GalleryText">City life in friend's pokemon world</span>
             <img className="GalleryImage" src={Malacruz} />
           </div>
         </Zoom>
 
         <Zoom>
           <div className="GalleryItem">
+            <span className="GalleryText">Kua in Carcaso</span>
             <img className="GalleryImage" src={Chriskua} />
           </div>
         </Zoom>
 
         <Zoom>
           <div className="GalleryItem">
+            <span className="GalleryText">Steel Dandelion</span>
             <img className="GalleryImage" src={Dandelion} />
           </div>
         </Zoom>
 
         <Zoom>
           <div className="GalleryItem">
+            <span className="GalleryText">Jinxxie in Carcaso</span>
             <img className="GalleryImage" src={Jinxie} />
           </div>
         </Zoom>
 
         <Zoom>
           <div className="GalleryItem">
+            <span className="GalleryText">Chris in Carcaso</span>
             <img className="GalleryImage" src={Kris} />
           </div>
         </Zoom>
 
         <Zoom>
           <div className="GalleryItem">
+            <span className="GalleryText">Peter in Carcaso</span>
             <img className="GalleryImage" src={Peter} />
           </div>
         </Zoom>
 
         <Zoom>
           <div className="GalleryItem">
+            <span className="GalleryText">We Must Dissent</span>
             <img className="GalleryImage" src={Rat} />
           </div>
         </Zoom>
 
         <Zoom>
           <div className="GalleryItem">
+            <span className="GalleryText">Asa in Carcaso</span>
             <img className="GalleryImage" src={Asa} />
           </div>
         </Zoom>
 
         <Zoom>
           <div className="GalleryItem">
+            <span className="GalleryText">Vampire Turtleneck</span>
             <img className="GalleryImage" src={Vampire} />
           </div>
         </Zoom>
