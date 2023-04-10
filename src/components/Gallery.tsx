09942c25244213577e21845b077d328771e983fd
Nicolas Crumrine <crumrinecoding@gmail.com>
2023-04-10 17:20:17 -0500

    Added more art, and cleaned up code to be Typescript frienldy

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,107 +1,82 @@
 import Zoom from "react-medium-image-zoom";
 
-const TammyPixelArt = require("../assets/tammypixelart.png");
-const Malacruz = require("../assets/malacruz.jpeg");
+import TammyPixelArt from "../assets/tammypixelart.png";
+import Malacruz from "../assets/malacruz.jpeg";
 
-const Nunn = require("../assets/Nunn.png");
-const Banner = require("../assets/twitter banner.png");
+import Nunn from "../assets/Nunn.png";
+import Banner from "../assets/twitter banner.png";
 
-const Chriskua = require("../assets/Carcaso/chriskua.jpeg");
-const Dandelion = require("../assets/Carcaso/dandelion.jpeg");
-const Jinxie = require("../assets/Carcaso/jinxie.jpeg");
-const Kris = require("../assets/Carcaso/kris.jpeg");
-const Peter = require("../assets/Carcaso/peter.jpeg");
-const Rat = require("../assets/Carcaso/ratjacket.jpeg");
-const Asa = require("../assets/Carcaso/spidermaidasa.jpeg");
-const Vampire = require("../assets/Carcaso/vampireturtleneck.jpeg");
+import Bee from "../assets/beelady.png";
+import wilsonPokemon from "../assets/wilson.png";
+import Travers from "../assets/travers.jpeg";
+
+import Chriskua from "../assets/Carcaso/chriskua.jpeg";
+import Dandelion from "../assets/Carcaso/dandelion.jpeg";
+import Jinxie from "../assets/Carcaso/jinxie.jpeg";
+import Kris from "../assets/Carcaso/kris.jpeg";
+import Peter from "../assets/Carcaso/peter.jpeg";
+import Rat from "../assets/Carcaso/ratjacket.jpeg";
+import Asa from "../assets/Carcaso/spidermaidasa.jpeg";
+import Vampire from "../assets/Carcaso/vampireturtleneck.jpeg";
+
+import RatLamp from "../assets/Carcaso/Ajax of Carcaso.png";
+import Balthasar from "../assets/Carcaso/Balthasar.png";
+import Bard from "../assets/Carcaso/BardCarcaso.png";
+import Deus from "../assets/Carcaso/Deus.png";
+import Ellysa from "../assets/Carcaso/Ellysa.png";
+import Fighter from "../assets/Carcaso/FighterCarcaso.png";
+import Leo from "../assets/Carcaso/LeoCarcaso.png";
+import Fenmes from "../assets/Carcaso/Fenmes.png";
+import Vitriol from "../assets/Carcaso/Vitriol.png";
+
+interface GalleryItemProps {
+  text: string;
+  imageSrc: string;
+}
+
+function GalleryItem(props: GalleryItemProps) {
+  return (
+    <Zoom>
+      <div className="GalleryItem">
+        <span className="GalleryText">{props.text}</span>
+        <img className="GalleryImage" src={props.imageSrc} />
+      </div>
+    </Zoom>
+  );
+}
 
 function Gallery() {
+  const items: GalleryItemProps[] = [
+    { text: "Nunn but Evangelion", imageSrc: Nunn },
+    { text: "Tami with my favourite Yu-Gi-Oh archetypes", imageSrc: Banner },
+    { text: "Tami in style of Fire Emblem", imageSrc: TammyPixelArt },
+    { text: "City life in friend's pokemon world", imageSrc: Malacruz },
+    { text: "Kua in Carcaso", imageSrc: Chriskua },
+    { text: "Steel Dandelion", imageSrc: Dandelion },
+    { text: "Jinxxie in Carcaso", imageSrc: Jinxie },
+    { text: "Chris in Carcaso", imageSrc: Kris },
+    { text: "Peter in Carcaso", imageSrc: Peter },
+    { text: "We Must Dissent", imageSrc: Rat },
+    { text: "Asa in Carcaso", imageSrc: Asa },
+    { text: "Vampire Turtleneck", imageSrc: Vampire },
+    { text: "Ajax", imageSrc: RatLamp },
+    { text: "Balthasar", imageSrc: Balthasar },
+    { text: "Bard", imageSrc: Bard },
+    { text: "Deus", imageSrc: Deus },
+    { text: "Ellysa", imageSrc: Ellysa },
+    { text: "Fighter", imageSrc: Fighter },
+    { text: "Leo", imageSrc: Leo },
+    { text: "Fenmes", imageSrc: Fenmes },
+    { text: "Vitriol", imageSrc: Vitriol },
+    { text: "Bee", imageSrc: Bee },
+  ];
   return (
     <div className="Gallery">
-      <div className="GalleryImagesContainer">
-        <Zoom>
-          <div className="GalleryItem">
-            <span className="GalleryText">Nunn but Evangelion</span>
-            <img className="GalleryImage" src={Nunn} />
-          </div>
-        </Zoom>
-        <Zoom>
-          <div className="GalleryItem">
-            <span className="GalleryText">Tami with my favourite Yu-Gi-Oh archetypes</span>
-            <img className="GalleryImage" src={Banner} />
-          </div>
-        </Zoom>
-        <Zoom>
-          <div className="GalleryItem">
-            <span className="GalleryText">Tami in style of Fire Emblem</span>
-            <img className="GalleryImage" src={TammyPixelArt} />
-          </div>
-        </Zoom>
-        <Zoom>
-          <div className="GalleryItem">
-            <span className="GalleryText">
-              City life in friend's pokemon world
-            </span>
-            <img className="GalleryImage" src={Malacruz} />
-          </div>
-        </Zoom>
-
-        <Zoom>
-          <div className="GalleryItem">
-            <span className="GalleryText">Kua in Carcaso</span>
-            <img className="GalleryImage" src={Chriskua} />
-          </div>
-        </Zoom>
-
-        <Zoom>
-          <div className="GalleryItem">
-            <span className="GalleryText">Steel Dandelion</span>
-            <img className="GalleryImage" src={Dandelion} />
-          </div>
-        </Zoom>
-
-        <Zoom>
-          <div className="GalleryItem">
-            <span className="GalleryText">Jinxxie in Carcaso</span>
-            <img className="GalleryImage" src={Jinxie} />
-          </div>
-        </Zoom>
-
-        <Zoom>
-          <div className="GalleryItem">
-            <span className="GalleryText">Chris in Carcaso</span>
-            <img className="GalleryImage" src={Kris} />
-          </div>
-        </Zoom>
-
-        <Zoom>
-          <div className="GalleryItem">
-            <span className="GalleryText">Peter in Carcaso</span>
-            <img className="GalleryImage" src={Peter} />
-          </div>
-        </Zoom>
-
-        <Zoom>
-          <div className="GalleryItem">
-            <span className="GalleryText">We Must Dissent</span>
-            <img className="GalleryImage" src={Rat} />
-          </div>
-        </Zoom>
-
-        <Zoom>
-          <div className="GalleryItem">
-            <span className="GalleryText">Asa in Carcaso</span>
-            <img className="GalleryImage" src={Asa} />
-          </div>
-        </Zoom>
-
-        <Zoom>
-          <div className="GalleryItem">
-            <span className="GalleryText">Vampire Turtleneck</span>
-            <img className="GalleryImage" src={Vampire} />
-          </div>
-        </Zoom>
-      </div>
+        <div className="GalleryImagesContainer">
+      {items.map((item, index) => (
+        <GalleryItem key={index} text={item.text} imageSrc={item.imageSrc} />
+      ))}
+    </div>
     </div>
   );
 }
